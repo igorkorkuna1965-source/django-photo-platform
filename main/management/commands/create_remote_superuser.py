52627de6e54076d8451fdec5e0321140ebaffde4
IhorK <igorkorkuna1965@gmail.com>
2026-03-08 15:15:03 +0200

    force update superuser password

--- main/management/commands/create_remote_superuser.py
+++ main/management/commands/create_remote_superuser.py
@@ -2,15 +2,22 @@ from django.core.management.base import BaseCommand
 from django.contrib.auth import get_user_model
 
 class Command(BaseCommand):
-    help = 'Create a superuser on remote server'
+    help = 'Create or update superuser on remote server'
 
     def handle(self, *args, **kwargs):
         User = get_user_model()
         username = 'Ihor5'
-        password = 'YourNewPassword123'  # постав свій пароль
+        password = 'Ihor12345'
         email = ''
-        if not User.objects.filter(username=username).exists():
-            User.objects.create_superuser(username=username, email=email, password=password)
+
+        user, created = User.objects.get_or_create(username=username, defaults={"email": email})
+
+        user.set_password(password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save()
+
+        if created:
             self.stdout.write(self.style.SUCCESS(f'Superuser {username} created'))
         else:
-            self.stdout.write(self.style.WARNING(f'Superuser {username} already exists'))
+            self.stdout.write(self.style.SUCCESS(f'Password for {username} updated'))
