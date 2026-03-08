4d573011df8ccbd42958308eb82b0ee50314d5fb
IhorK <igorkorkuna1965@gmail.com>
2026-03-08 13:59:42 +0200

    add remote superuser creation

--- build.sh
+++ build.sh
@@ -5,3 +5,5 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+python manage.py create_remote_superuser
+gunicorn mysite.wsgi:application --bind 0.0.0.0:$PORT
